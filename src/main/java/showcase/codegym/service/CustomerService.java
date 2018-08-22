5176c86d31417f2e44b4cbe0565e52854bf2a523
hoangviet <hoangviet1312@gmail.com>
2018-08-22 09:37:59 +0700

    add package service ( interface CustomerService + CustomerServiceImpl)

---
+++ src/main/java/showcase/codegym/service/CustomerService.java
@@ -0,0 +1,17 @@
+package showcase.codegym.service;
+
+import showcase.codegym.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
